8a465713bde6150da3f825ab9e1e7e5dfaf50e27
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-14 14:53:04 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'students', to: 'students#index'
+  get 'students/grades', to: 'students#grades'
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
+# Rails.application.routes.draw do
+#   resources :students do
+#     collection do
+#       get :grades
+#       get :highest_grade
+#     end
+#   end
+# end
